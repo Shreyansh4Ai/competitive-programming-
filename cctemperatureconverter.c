69513504fc8d6880317983f24dbdd2879edd1d53
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-01-28 15:32:39 +0530

    .

---
+++ cctemperatureconverter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+        float celsius=20.5;
+        float fahrenheit;
+        fahrenheit = (celsius * 9/5) + 32;
+        printf("%f",fahrenheit);
+        return 0;
+}
+
